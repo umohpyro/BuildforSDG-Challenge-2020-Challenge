0a9aa537df8d69fe1b75a9257acdc7215d0e13a9
umohpyro <andemumoh@gmail.com>
2020-04-08 09:40:13 +0100

    First estimator build

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,9 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    return{
+data: input,
+impact:{},
+servereImpact: {}
+    };
+};
 
 export default covid19ImpactEstimator;
